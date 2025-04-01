c1cdf30a86ba3bdd32ba79b13f45881e48b4001d
iaggocapitanio1 <iaggo.capitanio@gmail.com>
2025-04-01 10:22:55 -0300

    first commit

---
+++ video/target/debug/deps/video-9214c3b458f36ca8.d
@@ -0,0 +1,5 @@
+C:\Users\iaggo\Documents\Projects\tmp\video-processing\video\target\debug\deps\libvideo-9214c3b458f36ca8.rmeta: src\main.rs
+
+C:\Users\iaggo\Documents\Projects\tmp\video-processing\video\target\debug\deps\video-9214c3b458f36ca8.d: src\main.rs
+
+src\main.rs:
